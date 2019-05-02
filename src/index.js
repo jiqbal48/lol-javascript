be42436a6afe7eee44b6df6d491d1e8389ee60ba
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-05-02 14:00:11 -0400

    classes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ const a = function(x, y) {
 console.log(a(3, 2));
 
 // they can also be declared.
-function aa(x, y) {
+function b(x, y) {
   return x + y;
 }
-console.log(a(5, 2));
+console.log(b(5, 2));
+
+// so classes are basically functions. and can be defined same way
+// declaritive.
+class Table {
+  constructor(age) {
+    this.name = "tableo";
+    this.age = age;
+  }
+}
+const mrT = new Table(30); // also note, the `new`. cant call classes off the cuff. classes return object.
+console.log(mrT);
+// um.. another comment. classes are not hoisted as functions are. u can't use a class
+// before its defined.
